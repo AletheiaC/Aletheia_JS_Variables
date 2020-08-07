081fd44a6edde6bbadc8fc55e08bc580d2d3f3ea
AletheiaC <68302999+AletheiaC@users.noreply.github.com>
2020-08-07 15:59:10 -0500

    Index and variables.

---
+++ variables.js
@@ -0,0 +1,16 @@
+var num1 = 17;
+var str1 = "My Favorite";
+var phrase = `I am ${num1} years old`;
+var phrase2 = `${str1} color is Gray`;
+var phrase3 = `${str1} horror movie is Invisible Man`;
+var phrase4 = `${str1} superhero is Shazam`;
+var str2 = "No.";
+var phrase5 = `Am I taller than 5ft 5in? ${str2}`;
+
+
+console.log(phrase);
+console.log(phrase2);
+console.log(phrase3);
+console.log(phrase4);
+console.log(phrase5);
+
